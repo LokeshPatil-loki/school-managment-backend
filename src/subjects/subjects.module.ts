9410d487bc56ccff519664abad1dee9d53dd3ec7
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-19 18:38:46 +0530

    import pagination module in subjects module

--- src/subjects/subjects.module.ts
+++ src/subjects/subjects.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { SubjectsController } from './subjects.controller';
 import { SubjectsService } from './providers/subjects.service';
 import { FindSubjects } from './providers/find-subjects.provider';
+import { PaginationModule } from 'src/common/pagination/pagination.module';
 
 @Module({
   controllers: [SubjectsController],
   providers: [SubjectsService, FindSubjects],
+  imports: [PaginationModule],
 })
 export class SubjectsModule {}
