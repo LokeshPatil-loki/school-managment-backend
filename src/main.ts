bb5ffa7e61442315b0a2d965c59a41429c5301c4
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-30 13:47:15 +0530

    used cookie parser and configure cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
