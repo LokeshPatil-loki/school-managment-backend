3b0c692fb3ec3140d23bc1b939ef27cf7c1d6a2a
LokeshPatil-loki <lokesh1404@outlook.com>
2024-11-30 14:13:57 +0530

    Used ActiveUserDecorator

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { GetAssignmentsDto } from './dto/get-assignments.dto';
 import { query } from 'express';
 import { AssignmentsService } from './providers/assignments.service';
+import { ClerkAuthGuard } from 'src/auth/guards/clerk-auth/clerk-auth.guard';
+import { ActiveUserDecorator } from 'src/clerk/decorators/active-user.decorator.decorator';
+import { CurrentUser } from 'src/common/type/current-user.interface';
 
 @Controller('assignments')
 export class AssignmentsController {
   constructor(private readonly assignmentsService: AssignmentsService) {}
 
+  // @UseGuards(ClerkAuthGuard)
   @Get()
-  findExamService(@Query() query: GetAssignmentsDto) {
-    return this.assignmentsService.findExams(query);
+  async findExamService(
+    @Query() query: GetAssignmentsDto,
+    @ActiveUserDecorator() currentUser: CurrentUser,
+  ) {
+    const res = await this.assignmentsService.findExams(query, currentUser);
+    return res;
   }
 }
--- src/assignments/providers/assignments.service.ts
+++ src/assignments/providers/assignments.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { FindAssignmentsProvider } from './find-assignments.provider';
 import { GetAssignmentsDto } from '../dto/get-assignments.dto';
+import { CurrentUser } from 'src/common/type/current-user.interface';
 
 @Injectable()
 export class AssignmentsService {
   constructor(
     private readonly findAssignmentsProvider: FindAssignmentsProvider,
   ) {}
-  async findExams(query: GetAssignmentsDto) {
-    return await this.findAssignmentsProvider.findAssignments(query);
+  async findExams(query: GetAssignmentsDto, currentUser: CurrentUser) {
+    return await this.findAssignmentsProvider.findAssignments(
+      query,
+      currentUser,
+    );
   }
 }
